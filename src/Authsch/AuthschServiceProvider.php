27b2642a47ec07aa5fe24a81b31ac1735c776755
Sztyup <sztyup@gmail.com>
2017-06-08 04:13:39 +0200

    Refactor folder structure

--- src/Authsch/AuthschServiceProvider.php
+++ src/Authsch/AuthschServiceProvider.php
@@ -8,11 +8,18 @@ class AuthschServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        include __DIR__.'/routes.php';
         $this->app->make('Sztyup\Authsch\LoginController.php');
+
+        $this->loadRoutesFrom(__DIR__ . "routes/routes.php");
+
+        $this->mergeConfigFrom(
+            __DIR__.'/config/config.php', 'authsch'
+        );
     }
     public function boot()
     {
-
+        $this->publishes([
+            __DIR__ . '/config/config.php' => config_path('authsch.php')
+        ]);
     }
 }
