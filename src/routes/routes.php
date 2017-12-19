2163352f8e928172482f366171a12477603c4588
Sztyup <sztyup@gmail.com>
2017-12-19 02:03:14 +0100

    Delete routes

--- src/routes/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::group([
-    'middleware' => 'web',
-    'namespace' => '\\Sztyup\\Authsch'
-], function() {
-    Route::get('auth/sch', 'LoginController@redirect')->name('authsch.redirect');
-    Route::get('auth/sch/callback', 'LoginController@callback')->name('authsch.callback');
-});
