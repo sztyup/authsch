d96173ec465c1f1541eb3aa4d313a1374ee2e73c
Sztyup <sztyup@gmail.com>
2017-12-21 03:14:12 +0100

    Shacc model and migration

---
+++ src/Authsch/Model/SchAccount.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Sztyup\Authsch\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Sztyup\Authsch\Contracts\UserInterface;
+
+class SchAccount extends Model implements UserInterface
+{
+    protected $table = 'sch_accounts';
+
+    protected $fillable = [
+        'user_id',
+        'provider_user_id',
+        'access_token',
+        'refresh_token',
+        'name',
+        'bme_id',
+        'bme_status',
+        'dormitory',
+        'room_number',
+        'schacc',
+        'neptun',
+        'phone'
+    ];
+
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $id): UserInterface
+    {
+        $this->user_id = $id;
+
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): UserInterface
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): UserInterface
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/migrations/2017_12_20_194649_create_sch_accounts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchAccountsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('sch_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('provider_user_id');
+            $table->string('schacc');
+            $table->string('access_token');
+            $table->string('refresh_token');
+            $table->integer('bme_id');
+            $table->integer('bme_status');
+            $table->string('dormitory')->nullable();
+            $table->integer('room_number')->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('neptun')->nullable();
+            $table->timestamps();
+        });
+    }
+}
